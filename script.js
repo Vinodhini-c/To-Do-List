72e6a89754bd98c9d873b52658251a1f3088b33e
C Vinodhini <cvinodhini2@gmail.com>
2024-08-29 14:07:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var inpurBox = document.getElementById("input-box")
+var listcontainer = document.getElementById("list-container")
+function add()
+    {
+        if(inpurBox.value == '')
+        {
+            alert("You must write something!");
+        }
+        else{
+            let li = document.createElement("li");
+            li.innerHTML = inpurBox.value;
+            listcontainer.appendChild(li);
+            let span = document.createElement('span');
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);            
+        }
+        inputBox.value = "";
+    }
+
+    listcontainer.addEventListener("click",function(e)
+    {
+        if(e.target.tagName === "LI")
+         {
+                e.target.classList.toggle("checked");
+        }
+        else if(e.target.tagName === "SPAN")
+        {
+            e.target.parentElement.remove();
+        }   
+     }, false );
